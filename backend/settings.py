d53c68b4023d4d4c18d21766f114661799b21419
Pietrocvisentin@gmail.com <Pietrocvisentin@gmail.com>
2025-09-29 15:40:29 -0300

    a

--- backend/settings.py
+++ backend/settings.py
@@ -3,7 +3,9 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-placeholder")  # nosec
+SECRET_KEY = os.getenv(
+    "DJANGO_SECRET_KEY", "django-insecure-placeholder"
+)  # nosec
 
 DEBUG = True
 
