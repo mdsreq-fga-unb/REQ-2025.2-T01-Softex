d450930435a8d5efe58a37504c29ddb1fb418530
Felipe Pedroza <231026330@aluno.unb.br>
2025-10-02 08:19:58 -0300

    modules

---
+++ docs-astro/.astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_RyjbV from "../../assets/LOGO_SOFTEX_TAGLINE_ROXO_OFFLINE.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/LOGO_SOFTEX_TAGLINE_ROXO_OFFLINE.png?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_RyjbV]]);
+		
---
+++ docs-astro/.astro/content-modules.mjs
@@ -0,0 +1,11 @@
+
+export default new Map([
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/licoes/unidade-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Flicoes%2Funidade-1.mdx&astroContentModuleFlag=true")],
+["src/content/docs/visao/cenario.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fvisao%2Fcenario.mdx&astroContentModuleFlag=true")],
+["src/content/docs/visao/cronograma.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fvisao%2Fcronograma.mdx&astroContentModuleFlag=true")],
+["src/content/docs/visao/interacao.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fvisao%2Finteracao.mdx&astroContentModuleFlag=true")],
+["src/content/docs/visao/levantamento-parcial.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fvisao%2Flevantamento-parcial.mdx&astroContentModuleFlag=true")],
+["src/content/docs/visao/metodologia.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fvisao%2Fmetodologia.mdx&astroContentModuleFlag=true")],
+["src/content/docs/visao/solucao.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fvisao%2Fsolucao.mdx&astroContentModuleFlag=true")]]);
+		
