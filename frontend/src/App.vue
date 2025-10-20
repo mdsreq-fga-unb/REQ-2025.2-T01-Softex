7efc22dfee676ed054df45ad5c647d18fd2e427e
Felipe Pedroza <73255212+darkymeubem@users.noreply.github.com>
2025-10-20 20:07:37 -0300

    refactor(frontend): atualizado com as stacks do projeto
    - Vue + Vite + TypeScript

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,30 @@
-<template>
-  <div id="app">
-    <h1>Frontend Vue.js Inicial</h1>
-  </div>
-</template>
-
-<script>
-export default {
-  name: "App"
-};
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
