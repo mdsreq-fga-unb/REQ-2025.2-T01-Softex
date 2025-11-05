dce8a7a30c75f4599d55df22ea12c48695b02074
K.Richard <kaua.cavalcante771@gmail.com>
2025-11-05 16:47:25 -0300

    fix(cadeira): corrigir bugs críticos no modelo Cadeira

--- backend/cadeira/models.py
+++ backend/cadeira/models.py
@@ -8,13 +8,14 @@ class Cadeira(models.Model):
 ]
 
     id_cadeira = models.AutoField(primary_key=True)
-    sala = models.ForeignKey(Sala, on_delete=models.CASCADE, related_name='cadeiras', default='Livre',choices=STATUS_CHOICES)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Livre')
+    sala = models.ForeignKey(Sala, on_delete=models.CASCADE, related_name='cadeiras')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='desocupada')
 
     def __str__(self):
         return (f"Cadeira_id: {self.id_cadeira}\n"
-                f" Status: {self.status}\n"
-                f"Planta: {self.planta}\n")
+                f"Status: {self.status}\n"
+                f"sala: {self.sala.nome_sala}\n"
+                f"Planta: {self.sala.planta.nome}\n")
     class Meta:
         db_table = 'cadeira'
 
@@ -22,10 +23,12 @@ class Cadeira(models.Model):
     #=========================================================
     # validação de quantidade de cadeiras antes de salvar
 
-    def salvar_cadeiras(self, *args, **kwargs):
-        if self.sala.cadeiras.count() >= self.sala.capacidade:
-            raise ValueError("A sala atingiu sua capacidade máxima de cadeiras.")
-        super().save(args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.pk and self.sala:
+            if self.sala.cadeiras.count() >= self.sala.capacidade:
+                raise ValueError("A sala atingiu sua capacidade máxima de cadeiras.")
+            
+        super().save(*args, **kwargs)
         
 
 
