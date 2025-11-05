22a2365a8724eb48f0d3cf1cac6eef6cf422cc50
K.Richard <kaua.cavalcante771@gmail.com>
2025-11-05 18:26:23 -0300

    feat(sala): Adiciona campo 'tipo' que diferencia (estacao vs reuniao)

--- backend/sala/models.py
+++ backend/sala/models.py
@@ -1,30 +1,37 @@
 from django.db import models
 from planta.models import Planta
 
+TIPO_SALA_CHOICES = [
+    ('estacao', 'Estação de Trabalho'),
+    ('reuniao', 'Sala de Reunião'),
+]
+
 STATUS_CHOICES = [
-    ('ocupada', 'Ocupada'),
-    ('desocupada', 'Desocupada'),
+    ('ocupada','Ocupada'),
+    ('desocupada','Desocupada'),
 ]
 
 
 class Sala(models.Model):
     id_sala = models.AutoField(primary_key=True)
+    tipo_sala = models.CharField(max_length=10, choices=TIPO_SALA_CHOICES, default='estacao')
+    
     nome_sala = models.CharField(max_length=45)
     capacidade = models.IntegerField(default=0)
     status = models.CharField(max_length=45, choices=STATUS_CHOICES)
     descricao = models.CharField(blank=True, max_length=200)
-    planta = models.ForeignKey(
-        Planta, on_delete=models.CASCADE, related_name="salas"
-    )
+    planta = models.ForeignKey(Planta, on_delete=models.CASCADE, related_name="salas")
 
 
     def __str__(self):
+        tipo_display = self.get_tipo_display()
         return (
             f"Sala_id: {self.id_sala}\n"
+            f"Tipo: {tipo_display}\n"
             f"Nome: {self.nome_sala}\n"
             f"Capacidade: {self.capacidade}\n"
             f"Status: {self.status}\n"
-            f"Planta_id: {self.planta.id}\n"
+            f"Planta_id: {self.planta.id_planta}\n"
         )
 
     class Meta:
