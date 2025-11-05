698bbcf745fbb340bce4ffdec0d2e16447dbf44c
K.Richard <kaua.cavalcante771@gmail.com>
2025-11-05 12:06:07 -0300

    Alteração do sqlite para o PostgreSQL e sua respectiva configuração e adição do .gitignore

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'softex_db',
+        'USER': 'user_admin',        
+        'PASSWORD': '1234',
+        #'HOST': 'localhost',
+        #'PORT': '5432',
     }
 }
 
