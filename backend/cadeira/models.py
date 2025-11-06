30946ec418e4bcf2fecaaa7a01c2e77e4316f7f6
rich4rd1 <richard.cavalcante771@gmail.com>
2025-11-06 01:06:05 -0300

    feat(cadeira) Adiciona coordenadas pos_x e pos_y para cadeiras

--- backend/cadeira/models.py
+++ backend/cadeira/models.py
@@ -10,12 +10,18 @@ class Cadeira(models.Model):
     id_cadeira = models.AutoField(primary_key=True)
     sala = models.ForeignKey(Sala, on_delete=models.CASCADE, related_name='cadeiras')
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='desocupada')
+    
+    pos_x = models.FloatField(default=0)
+    pos_y = models.FloatField(default=0)
+
 
     def __str__(self):
         return (f"Cadeira_id: {self.id_cadeira}\n"
                 f"Status: {self.status}\n"
                 f"sala: {self.sala.nome_sala}\n"
                 f"Planta: {self.sala.planta.nome}\n")
+    
+
     class Meta:
         db_table = 'cadeira'
 
