c5c423095c953ce9326660b39c3f67b5584d8e47
Felipe Pedroza <231026330@aluno.unb.br>
2025-11-13 09:28:57 -0300

    chore(gh-pages): atualização do documento da gitpages

--- docs-astro/astro.config.mjs
+++ docs-astro/astro.config.mjs
@@ -49,38 +49,41 @@ export default defineConfig({
 					label: "Visões de Produto e de Projeto",
 					items: [
 						{
-							label: "Contexto e Necessidades",
+							label: "Contexto",
 							slug: "visao/cenario",
 						},
 						{
-							label: "Solução Proposta",
+							label: "Solução",
 							slug: "visao/solucao",
 						},
-					
 						{
-							label: "Requisitos do Sistema",
-							slug: "visao/levantamento-parcial",
-						},
-						{
-							label: "Cronograma e Entregas",
-							slug: "visao/cronograma",
+							label: "Processo de Engenharia de Software",
+							slug: "visao/processo-sw",
 						},
 						{
 							label: "Comunicação e Colaboração",
 							slug: "visao/interacao",
 						},
 						{
-							label: "Backlog do Produto",
-							slug: "visao/backlog",
+							label: "Cronograma",
+							slug: "visao/cronograma",
 						},
 						{
-							label: "Engenharia de Requisitos",
+							label: "Processo de Engenharia de Requisitos",
 							slug: "visao/engenharia-requisitos",
 						},
-					{
-						label: "DoR & DoD",
-						slug: "visao/dor-dod",
-					},
+						{
+							label: "DoR / DoD",
+							slug: "visao/dor-dod",
+						},
+						{
+							label: "Requisitos de Software",
+							slug: "visao/levantamento-parcial",
+						},
+						{
+							label: "Backlog",
+							slug: "visao/backlog",
+						},
 					],
 				},
 			{
