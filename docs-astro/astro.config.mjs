be05eedfd45c3eeb7a9483f5ddf4382f309258da
Felipe Pedroza <231026330@aluno.unb.br>
2025-11-13 09:06:37 -0300

    update(gh-pages): refatoração dos det alhes dos requisiitos
    - adicionado lib de icones

--- docs-astro/astro.config.mjs
+++ docs-astro/astro.config.mjs
@@ -11,6 +11,18 @@ export default defineConfig({
 		starlight({
 			plugins: [starlightThemeRapidePlugin()],
 			title: "Softex - Sistema de Gestão de Recursos",
+			head: [
+				{
+					tag: "link",
+					attrs: {
+						rel: "stylesheet",
+						href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css",
+						integrity: "sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==",
+						crossorigin: "anonymous",
+						referrerpolicy: "no-referrer"
+					}
+				}
+			],
 			social: [
 				{
 					icon: "github",
@@ -65,27 +77,39 @@ export default defineConfig({
 							label: "Engenharia de Requisitos",
 							slug: "visao/engenharia-requisitos",
 						},
+					{
+						label: "DoR & DoD",
+						slug: "visao/dor-dod",
+					},
 					],
 				},
-				{
-					label: "Lições Aprendidas",
-					items: [
-						{
-							label: "Unidade 1",
-							slug: "licoes/unidade-1",
-						},
-					],
-				},
-			
-				{
-					label: "Entregas",
-					items: [
-						{
-							label: "Unidade 1",
-							slug: "entregas/unidade-1",
-						},
-					],
-				},
+			{
+				label: "Lições Aprendidas",
+				items: [
+					{
+						label: "Unidade 1",
+						slug: "licoes/unidade-1",
+					},
+					{
+						label: "Unidade 2",
+						slug: "licoes/unidade-2",
+					},
+				],
+			},
+		
+			{
+				label: "Entregas",
+				items: [
+					{
+						label: "Unidade 1",
+						slug: "entregas/unidade-1",
+					},
+					{
+						label: "Unidade 2",
+						slug: "entregas/unidade-2",
+					},
+				],
+			},
 			],
 		}),
 	],
