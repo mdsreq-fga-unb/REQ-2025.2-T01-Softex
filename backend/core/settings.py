364bf82bf646411573efa15c23d10b2bc2da69bd
y123yuri <dayannadir.2011@gmail.com>
2025-12-02 05:58:13 -0300

    bora

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -182,6 +182,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8080",
+    "http://35.94.90.225:5173",
+    "http://localhost:5173",
+
 ]
 
 CORS_ALLOW_CREDENTIALS = True
